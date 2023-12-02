c2ba7b207d8c5f22741325bdacef7f3eda8111d9
Lee Denbigh <leedenbigh@googlemail.com>
2023-12-02 20:17:30 +0000

    Removed previous attempt at creating the skills bar.

---
+++ LeeDenbighsInteractiveCV/Models/PersonalInfoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeeDenbighsInteractiveCV.Models
+{
+    public class PersonalInfoModel
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
--- LeeDenbighsInteractiveCV/Views/VisualSkillsView.xaml.cs
+++ LeeDenbighsInteractiveCV/Views/VisualSkillsView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using LeeDenbighsInteractiveCV.ViewModels;
+﻿using LeeDenbighsInteractiveCV.Models;
+using LeeDenbighsInteractiveCV.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -26,7 +28,16 @@ namespace LeeDenbighsInteractiveCV.Views
         {
             InitializeComponent();
 
+            // This line sets the data context of the current UserControl 
+            // to an instance of the SkillViewModel class.
+            //
+            // By setting the SkillViewModel instance as the DataContext, all data bindings in the XAML
+            // that rely on the DataContext will now be bound to properties and commands of the SkillViewModel.
+            // For example, if your SkillViewModel has a property called 'Skills', and your XAML has a
+            // ListBox with its ItemsSource property bound to 'Skills', the ListBox will display data
+            // from the SkillViewModel's 'Skills' property.
             this.DataContext = new SkillViewModel();
         }
+
     }
 }
